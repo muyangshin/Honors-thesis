4cbd13505c11b314158383189f09e28bfa2b5a9f
muyangshin <myang752@gmail.com>
2018-04-22 18:36:45 -0700

    Resolve #2

---
+++ code/oes.R
@@ -0,0 +1,183 @@
+library(tidyverse)
+
+source("code/naics.R")
+
+# Kilcoyne
+high.tech.occupations <- c("11-302", "11-904", "11-912", "15-", "17-2", "17-3", "19-1", "19-2", "19-4", "27-1014", "29-2011",
+                           "29-2033", "29-2034")
+
+tech.group.occupations <- c("15-", "17-", "19-", "29-")
+
+# BLS
+bls.stem.occupations <- c("11-302", "11-904", "11-912", "15-", "17-102", "17-2", "17-3", "19-1", "19-2",
+                          "19-301", "19-3022", "19-303", "19-304", "19-305", "19-309", "19-4", "41-903")
+
+bls.stem_related.occupations <- c(bls.stem.occupations, "11-911", "17-", "29-")
+
+
+ReadOES <- function(year, digits = 4, save = F) {
+  # Read OES
+  #
+  # Args:
+  #   year: 
+  #
+  #  Returns:
+  #   data frame
+  
+  if (year >= 2008) {
+    month.str <- "M"
+  } else if (year == 2007) {
+    month.str <- "May"
+  } else if (year >= 2003) {
+    month.str <- "may"
+  } else {
+    month.str <- ""
+  }
+  
+  df <- read.csv(paste0("data/OES/", "nat", digits, "d_", month.str, year, "_dl.csv"), 
+                 stringsAsFactors = F, encoding="UTF-8")
+  
+  colnames(df)[1] <- "NAICS"
+  if (year <= 2002) {
+    colnames(df) <- sapply(colnames(df), toupper)
+  }
+  
+  df <- df %>% 
+    mutate(
+      TOT_EMP = ifelse(TOT_EMP == "**", NA, TOT_EMP),
+      TOT_EMP = as.numeric(TOT_EMP)
+      ) %>%
+    filter(
+      # drop government jobs
+      NAICS < 999000,
+      
+      # drop empty tot_emp
+      !is.na(TOT_EMP)
+    )
+  
+  return(df)
+}
+
+CompileOES <- function(years, occupations, digits = 4, save = F, convert2002 = F) {
+  # Read OES using the specified occupation grouping
+  #
+  # Args:
+  #   years: single year / a vector of years
+  #   occupations: vector
+  #
+  #  Returns:
+  #   data frame
+  #   year  NAICS empl  tech tech.pct tech.pct.economy selected
+  
+  ConvertTo2002NAICS_OES <- function(df, year) {
+    if (year >= 2008) {
+      if (year >= 2017) {
+        conversion.list <- naics.2017.to.2012
+      } else if (year >= 2012) {
+        conversion.list <- naics.2012.to.2007
+      } else {
+        conversion.list <- naics.2007.to.2002
+      }
+  
+      df2 <- df %>%
+        filter(NAICS %in% names(conversion.list)) %>%
+        mutate(old = TRUE)
+  
+      df3 <- df2
+      for (i in 1:nrow(df2)) {
+        old.naics <- as.character(df2[i, "NAICS"])
+        for (j in seq_along(names(conversion.list[[old.naics]]))) {
+          this.naics = names(conversion.list[[old.naics]])[j]
+          this.prop = conversion.list[[old.naics]][[j]]
+  
+          df3 <- df3 %>%
+            rbind(list(
+              this.naics,
+              as.numeric(this.prop * df2[i, "empl"]),
+              as.numeric(this.prop * df2[i, "tech"]),
+              FALSE
+            ))
+        }
+      }
+  
+      df <- df3 %>%
+        filter(old == FALSE) %>%
+        select(-old) %>%
+        rbind(df %>% filter(!NAICS %in% df2$NAICS)) %>%
+        group_by(NAICS) %>%
+        summarise(empl = sum(empl),
+                  tech = sum(tech)
+        )
+  
+      # If it uses 2002 NAICS, needs to be converted twice
+      if (year >= 2017) {
+        df <- ConvertTo2002NAICS_OES(df, 2012)
+      } else if (year >= 2012) {
+        df <- ConvertTo2002NAICS_OES(df, 2008)
+      }
+    }
+    
+    return(df)
+  }
+  
+  
+  if (length(years) > 1) {
+    # Multiple Years
+    df <- CompileOES(years[1], occupations, digits = digits, convert2002 = convert2002)
+    
+    for (year in tail(years, -1)) {
+      df <- rbind(df, CompileOES(year, occupations, digits = digits, convert2002 = convert2002))
+    }
+    
+    if (save) {
+      write.csv(df, "data_out/OES_by_industry_total.csv")
+    }  
+    
+    return(df)
+    
+  } else {
+    # Single Year
+    year <- years
+    
+    # Read --------------------------------------------------------------------
+    
+    df <- ReadOES(year, digits)
+    
+    # is.subset ---------------------------------------------------------------
+    
+    # Filter out OCC_CODE that ends with 0: leave only "detailed"
+    df <- df %>% 
+      #is.subset
+      filter(!grepl("0+$", OCC_CODE)) %>%
+      mutate(is.subset = grepl(paste0("^", paste0(occupations, collapse = "|")), OCC_CODE)) %>%
+      mutate(tech = TOT_EMP * is.subset) %>%
+    
+      # Collapse by NAICS
+      group_by(NAICS) %>%
+      summarise(empl = sum(TOT_EMP), 
+                tech = sum(tech)
+      )
+    
+    # NAICS conversion
+    if (digits == 4 & convert2002) {
+      if (year <= 2008) {
+        df <- ConvertTo2002NAICS_OES(df, year)
+      }
+    }
+    
+    
+    # fractions ---------------------------------------------------------------
+    df <- df %>%
+      mutate(tech.pct = tech / empl, 
+             tech.pct.economy = sum(tech) / sum(empl),
+             tech.pct.sd = sd(tech.pct),
+             # selected = ifelse(tech.pct >= 2 * tech.pct.economy, 1, 0),
+             year = year,
+             NAICS = as.character(NAICS)) %>%
+      
+      # year as the first column
+      select(year, everything())
+    
+    return(df)
+  }
+}
--- code/tools.R
+++ code/tools.R
@@ -3,6 +3,8 @@ library(ipumsr)
 library(sqldf)
 library(RSQLite)
 
+source("code/oes.R")
+
 # Read IPUMS CPS data and write to SQLite
 write_ipums_to_sql <- function() {
   # READ IPUMS DATA ---------------------------------------------------------
@@ -99,3 +101,119 @@ load_df <- function() {
 
   return(df)
 }
+
+# create mapping from INDLY to OES industry codes,
+# and calculate economy average and sd using the given set of occupations
+prepare_INDLY_conversion_census_to_naics <- function(years, tech_occupations, df_oes = NULL) {
+  # fetch empl, tech from oes using the census_code
+  fetch_oes_empl_tech <- function(census_code, df) {
+    # census_code: number
+    # df: OES data, already filter by year
+    return(as.matrix(
+      df %>%
+        filter(startsWith(NAICS, census_code)) %>%
+        summarise(
+          empl = sum(empl),
+          tech = sum(tech)
+          ) %>%
+        select(empl, tech)
+    ))
+  }
+  
+  # construct df_oes only once
+  if (is.null(df_oes)) {
+    df_oes <- CompileOES(years, tech_occupations)
+  }
+  
+  if (length(years) > 1) {
+    # Multiple Years
+    df <- prepare_INDLY_conversion_census_to_naics(years[1], tech_occupations, df_oes)
+    
+    for (year in tail(years, -1)) {
+      df <- rbind(df, prepare_INDLY_conversion_census_to_naics(year, tech_occupations, df_oes))
+    }
+    
+    return(df)
+  } else {
+    # Single year
+    year <- years
+    
+    # Convert census industry code to sic/naics as in OES
+    if (year <= 2001) {
+      # TODO: create the file
+      filename <- "data/NAICS/1990_census_to_sic.csv"
+    } else if (year == 2002) {
+      filename <- "data/NAICS/1990_census_to_2002_naics.csv"
+    } else if (year <= 2007) {
+      filename <- "data/NAICS/2002_census_to_2002_naics.csv"
+    } else if (year == 2008) {
+      filename <- "data/NAICS/2002_census_to_2007_naics.csv"
+    } else if (year <= 2011) {
+      filename <- "data/NAICS/2007_census_to_2007_naics.csv"
+    } else if (year <= 2013) {
+      filename <- "data/NAICS/2007_census_to_2012_naics.csv"
+    } else if (year <= 2016) {
+      filename <- "data/NAICS/2012_census_to_2012_naics.csv"
+    } else {
+      # TODO: create the file
+      filename <- "data/NAICS/2012_census_to_2017_naics.csv"
+    }
+    
+    # long format, remove duplicates
+    df_conversion <- read.csv(filename, stringsAsFactors = F) %>%
+      setNames(c("INDLY", "naics")) %>%
+      separate(naics, paste0("naics", 1:15), ", ") %>%
+      mutate_at(vars(starts_with("naics")), funs(substr(., 1, 4))) %>%
+      gather("var", "naics", 2:16) %>%
+      filter(!is.na(naics)) %>%
+      group_by(INDLY, naics) %>%
+      summarise(var = first(var)) %>%
+      ungroup()
+    
+    # tech.pct.economy, tech.pct.sd
+    this_year_tech_pct_economy <- as.numeric(
+      df_oes %>% 
+        filter(year == !!year) %>%
+        summarise(tech.pct.economy = first(tech.pct.economy))
+    )
+    
+    this_year_tech_pct_sd <- as.numeric(
+      df_oes %>% 
+        filter(year == !!year) %>%
+        summarise(tech.pct.sd = first(tech.pct.sd))
+    ) 
+    
+    df_conversion <- df_conversion %>%
+      # link to oes
+      cbind(
+        as.data.frame(
+          t(sapply(as.character(df_conversion$naics),
+                   fetch_oes_empl_tech,
+                   df_oes %>% filter(year == !!year),
+                   USE.NAMES = F
+                   )
+            )
+          ) %>%
+          setNames(c("empl", "tech"))
+        ) %>%
+      
+      # compute fractions
+      group_by(INDLY) %>%
+      summarise(empl = sum(empl),
+                tech = sum(tech)) %>%
+      filter(empl > 0) %>%
+      mutate(tech.pct = tech / empl) %>%
+      
+      # create columns for year-specific values
+      mutate(
+        year = !!year,
+        tech.pct.economy = this_year_tech_pct_economy,
+        tech.pct.sd = this_year_tech_pct_sd
+        ) %>%
+      
+      # reorder columns
+      select(year, everything())
+    
+    return(df_conversion)
+  }
+}  

07a30ca34e389b06a02025f7a2214b7b69b2f3a2
muyangshin <myang752@gmail.com>
2018-04-25 05:38:41 -0700

    Change data source to CPS ORG

--- code/distribution_regression.R
+++ code/distribution_regression.R
@@ -1,20 +1,20 @@
 library(tidyverse)
-library(sqldf)
+library(sqldf)  # SQL
 
 source("code/tools.R")
 
 
 # LOAD DATA ---------------------------------------------------------------
 
-df_0 <- load_df()
+# df_0 <- load_df()
 
 # start year, end year
 years <- c(2002, 2016)
 
-# INDLY to naics
-df_INDLY_naics <- prepare_INDLY_conversion_census_to_naics(years, bls.stem.occupations)
+# IND to naics
+df_IND_naics <- prepare_INDLY_conversion_census_to_naics(years, occs_bls_stem, cached = T)
   
-df <- df_0 %>%
+df <- df_org_0 %>%
   filter(YEAR %in% years) %>%
   
   mutate(cons = 1) %>%
@@ -38,7 +38,7 @@ df <- df %>%
   mutate(METAREA = factor(METAREA)) %>%
   
   # tech using indsutry sector
-  left_join(df_INDLY_naics, by = c("YEAR", "INDLY")) %>%
+  left_join(df_IND_naics, by = c("YEAR", "IND")) %>%
   filter(!is.na(tech.pct)) %>%
   mutate(is_high_tech_ind = ifelse(tech.pct >= tech.pct.economy + tech.pct.sd, 1, 0))
   
@@ -51,11 +51,12 @@ df <- df %>%
 # ANALYSIS ----------------------------------------------------------------
 
 # set thresholds for lwage: approx from -8 to 8
-lwage_thresholds <- seq(min(df$lwage), max(df$lwage), by = 0.8)
+# lwage_thresholds <- seq(min(df$lwage), max(df$lwage), by = 0.8)
+lwage_thresholds <- quantile(df$lwage, seq(0.02, 0.98, by = 0.001))[-1]
 
 # regression specification
 tech <- "is_stem"
-xs <- c("schooling", "experience", "experience_sq", "married", "METAREA")
+xs <- c("schooling", "experience", "experience_sq", "married")
 
 xs_full <- c()  # vector of control variable names, including dummy variables
 for (x in xs) {
@@ -74,108 +75,277 @@ for (x in xs) {
   }
 }
 
+
+# Construct covariate distributions ---------------------------------------
+
+quantile_interval <- 0.1
+
+construct_empirical_pdf <- function(vars, vecs, data) {
+  # number of cells
+  num <- 1
+  for (vec in vecs) {
+    num <- num * length(vec)
+  }
+  
+  # construct df_max, df_min
+  df_max <- as.data.frame(matrix(ncol = length(vecs),
+                                 nrow = num))
+  df_min <- as.data.frame(matrix(ncol = length(vecs),
+                                 nrow = num))
+  for (i in 1:num) {
+    row_max <- vector("list", length = length(vecs))
+    row_min <- vector("list", length = length(vecs))
+    prevs <- 1
+    for (j in seq_along(vecs)) {
+      vec <- vecs[[j]]
+      
+      row_max[[j]] <- vec[((i - 1) %/% prevs) %% length(vec) + 1]
+      
+      if (((i - 1) %/% prevs) %% length(vec) + 1 == 1) {
+        row_min[[j]] <- -Inf
+      } else {
+        row_min[[j]] <- vec[((i - 1) %/% prevs) %% length(vec)]
+      }
+      
+      prevs <- prevs * length(vec)
+    }
+    
+    df_max[i, ] <- row_max
+    df_min[i, ] <- row_min
+  }
+  
+  # construct df
+  result <- numeric(num)
+  
+  for (i in 1:num) {
+    queries <- c()
+    for (j in seq_along(vecs)) {
+      queries <- c(queries, 
+                   paste0(vars[[j]], " <= ", df_max[i, j]),
+                   paste0(vars[[j]], " > ", df_min[i, j])
+                   )
+    }
+    
+    queries_string <- paste0(queries, collapse = " & ")
+    
+    result[i] <- nrow(data %>% filter_(queries_string))
+  }
+  
+  return(as.matrix(cbind(1, df_max, result / nrow(data))))
+}
+
+# f_x0
+f_x0 <- construct_empirical_pdf(
+  c(tech, xs_full),
+  list(
+    c(0, 1),
+    quantile(df$schooling, seq(0, 1, by = quantile_interval))[-1],
+    quantile(df$experience, seq(0, 1, by = quantile_interval))[-1],
+    quantile(df$experience_sq, seq(0, 1, by = quantile_interval))[-1],
+    c(0, 1)
+  ),
+  data = df %>% filter(YEAR == years[1])
+)
+
+# f_c1
+f_c1 <- construct_empirical_pdf(
+  c(xs_full),
+  list(
+    quantile(df$schooling, seq(0, 1, by = quantile_interval))[-1],
+    quantile(df$experience, seq(0, 1, by = quantile_interval))[-1],
+    quantile(df$experience_sq, seq(0, 1, by = quantile_interval))[-1],
+    c(0, 1)
+  ),
+  data = df %>% filter(YEAR == years[2])
+)
+
+# # F_X0, F_X1, F_C1
+# cov_distributions <- vector("list", length = 3)
+# 
+# # TODO: automated
+# a <- as.matrix(expand.grid(
+#   c(0, 1),
+#   quantile(df$schooling, seq(0, 1, by = quantile_interval))[-1],
+#   quantile(df$experience, seq(0, 1, by = quantile_interval))[-1],
+#   quantile(df$experience_sq, seq(0, 1, by = quantile_interval))[-1],
+#   c(0, 1)
+# ))
+# 
+# # without tech
+# a_C <- as.matrix(expand.grid(
+#   quantile(df$schooling, seq(0, 1, by = quantile_interval))[-1],
+#   quantile(df$experience, seq(0, 1, by = quantile_interval))[-1],
+#   quantile(df$experience_sq, seq(0, 1, by = quantile_interval))[-1],
+#   c(0, 1)
+# ))
+# 
+# # X0, X1
+# for (i in seq_along(years)) {
+#   t <- years[i]
+#   df_t <- df %>% 
+#     filter(YEAR == t) %>%
+#     select(!!tech, !!xs_full)
+#   
+#   obj <- initF(as.matrix(df_t), 2)
+#   
+#   cov_distributions[[i]] <- emcdf(obj, a)
+# }
+# 
+# # C1
+# t <- years[2]
+# df_t <- df %>% 
+#   filter(YEAR == t) %>%
+#   select(!!xs_full)
+# obj <- initF(as.matrix(df_t), 2)
+# cov_distributions[[3]] <- emcdf(obj, a_C)
+
+
+# Counterfactual Y1 given X1 ----------------------------------------------
+
 # construct empty data frame to save regression coefficients
 df_reg <- data.frame(matrix(ncol = 4 + length(xs_full), 
-                            nrow = length(years) * length(lwage_thresholds))) %>%
+                            nrow = length(lwage_thresholds))) %>%
   setNames(c("year", "lwage", "cons", tech, xs_full))
 
 # distribution regression
-for (i in seq_along(years)) {
-  t <- years[i]
-  df_t <- df %>% filter(YEAR == t)
+t <- years[2]  # use the last year
+df_t <- df %>% filter(YEAR == t)
+
+for (j in seq_along(lwage_thresholds)) {
+  threshold <- lwage_thresholds[j]
   
-  for (j in seq_along(lwage_thresholds)) {
-    threshold <- lwage_thresholds[j]
-    
-    y <- df_t$lwage <= threshold
-    reg_formula <- paste0("y ~ ", paste0(c(tech, xs_full), collapse = " + "))
-    reg <- lm(reg_formula, data = df_t, weights = weight)
-    
-    df_reg[(i - 1) * length(lwage_thresholds) + j, ] <- c(t, threshold, reg$coefficients)
-  }
+  y <- df_t$lwage <= threshold
+  reg_formula <- paste0("y ~ ", paste0(c(tech, xs_full), collapse = " + "))
+  reg <- lm(reg_formula, data = df_t, weights = weight)
+  
+  df_reg[j, ] <- c(t, threshold, reg$coefficients)
 }
+
+
+# conditional distribution u0 given c0 ------------------------------------
+
+reg_formula <- paste0(tech, " ~ ", paste0(xs_full, collapse = " + "))
+
+reg_u0_c0 <- glm(reg_formula, 
+                 data = df %>% filter(YEAR == years[1]), 
+                 family = "binomial",
+                 weights = weight)
+
+
+# construct distributions -------------------------------------------------
 
 # empty data frame to store cdfs
 df_cdf <- data.frame(
-  year = numeric(length(years) * length(lwage_thresholds)),
-  lwage = numeric(length(years) * length(lwage_thresholds)),
-  cdf = numeric(length(years) * length(lwage_thresholds)),
-  cdf_tech = numeric(length(years) * length(lwage_thresholds)),
-  cdf_cont = numeric(length(years) * length(lwage_thresholds))
+  year = numeric(length(lwage_thresholds)),
+  lwage = numeric(length(lwage_thresholds)),
+  cdf = numeric(length(lwage_thresholds)),
+  cdf_tech = numeric(length(lwage_thresholds)),
+  cdf_cont = numeric(length(lwage_thresholds))
 )
 
-# construct cdfs
-for (i in seq_along(years)) {
-  t <- years[i]
-  df_t <- df %>% filter(YEAR == t)
-  N_t <- nrow(df_t)
-  
-  A_t <- df_t %>% 
-    mutate_at(c("cons", tech, xs_full), funs(. * weight)) %>%
-    select(cons, !!tech, !!xs_full) %>% 
-    summarise_all(sum)
-  
-  A_tech <- A_t %>% 
-    mutate(
-      !!tech := t(as.matrix(df[df$YEAR == years[1], tech])) %*% as.matrix(df[df$YEAR == years[1], "weight"])
-      )
-  A_cont <- as.matrix(A_tech)
-  A_cont[3:(2 + length(xs_full))] <- as.matrix(df %>% 
-                                            filter(YEAR == years[1]) %>%
-                                            mutate_at(xs_full, funs(. * weight)) %>%
-                                            select(!!xs_full) %>% 
-                                            summarise_all(funs(sum)))
-  
-  A_t <- as.matrix(A_t)
-  A_tech <- as.matrix(A_tech)
-  A_cont <- as.matrix(A_cont)
-  
-  for (j in seq_along(lwage_thresholds)) {
-    threshold <- lwage_thresholds[j]
+df_cdf$year <- years[2]
+df_cdf$lwage <- lwage_thresholds
+df_cdf$cdf <- seq(0.02, 0.98, by = 0.96 / length(lwage_thresholds))[-1]
 
-    B <- t(as.matrix(df_reg[(i - 1) * length(lwage_thresholds) + j, c("cons", tech, xs_full)]))
-    
-    df_cdf[(i - 1) * length(lwage_thresholds) + j, ] <- c(t,
-                                                          threshold,
-                                                          A_t %*% B,
-                                                          A_tech %*% B,
-                                                          A_cont %*% B
-                                                          )
-  }
+# fitted values of logistic regression
+reg_u0_c0_fitted <- f_x0[, -c(2, ncol(f_x0))] %*% reg_u0_c0$coefficients
+reg_u0_c0_fitted <- exp(reg_u0_c0_fitted) / (1 + exp(reg_u0_c0_fitted))
+reg_u0_c0_fitted <- ifelse(f_x0[, 2] == 0, 1 - reg_u0_c0_fitted, reg_u0_c0_fitted)
+
+for (i in 1:nrow(df_reg)) {
+  # counterfactual
+  vec_coeffs <- t(df_reg[i, -(1:2)])
+  
+  df_cdf[i, "cdf_tech"] <- sum((f_x0[, -ncol(f_x0)] %*% vec_coeffs) * reg_u0_c0_fitted * rep(f_c1[, ncol(f_c1)], each = 2))
+  df_cdf[i, "cdf_cont"] <- sum((f_x0[, -ncol(f_x0)] %*% vec_coeffs) * f_x0[, ncol(f_x0)])
 }
 
+
+# # construct cdfs
+# for (i in seq_along(years)) {
+#   t <- years[i]
+#   df_t <- df %>% filter(YEAR == t)
+#   N_t <- nrow(df_t)
+#   
+#   A_t <- df_t %>% 
+#     mutate_at(c("cons", tech, xs_full), funs(. * weight)) %>%
+#     select(cons, !!tech, !!xs_full) %>% 
+#     summarise_all(sum)
+#   
+#   A_tech <- A_t %>% 
+#     mutate(
+#       !!tech := t(as.matrix(df[df$YEAR == years[1], tech])) %*% as.matrix(df[df$YEAR == years[1], "weight"])
+#       )
+#   A_cont <- as.matrix(A_tech)
+#   A_cont[3:(2 + length(xs_full))] <- as.matrix(df %>% 
+#                                             filter(YEAR == years[1]) %>%
+#                                             mutate_at(xs_full, funs(. * weight)) %>%
+#                                             select(!!xs_full) %>% 
+#                                             summarise_all(funs(sum)))
+#   
+#   A_t <- as.matrix(A_t)
+#   A_tech <- as.matrix(A_tech)
+#   A_cont <- as.matrix(A_cont)
+#   
+#   for (j in seq_along(lwage_thresholds)) {
+#     threshold <- lwage_thresholds[j]
+# 
+#     B <- t(as.matrix(df_reg[(i - 1) * length(lwage_thresholds) + j, c("cons", tech, xs_full)]))
+#     
+#     df_cdf[(i - 1) * length(lwage_thresholds) + j, ] <- c(t,
+#                                                           threshold,
+#                                                           A_t %*% B,
+#                                                           A_tech %*% B,
+#                                                           A_cont %*% B
+#                                                           )
+#   }
+# }
+
 # calculate variance
-df_results <- df_cdf %>%
-  group_by(year) %>%
+df_cdf <- df_cdf %>%
   mutate(
     d_cdf = cdf - lag(cdf),
     d_cdf_tech = cdf_tech - lag(cdf_tech),
     d_cdf_cont = cdf_cont - lag(cdf_cont)
     ) %>%
-  filter(!is.na(d_cdf)) %>%
-  summarise(
-    e = sum(lwage * d_cdf),
-    sd = sqrt(sum(lwage^2 * d_cdf) - sum(lwage * d_cdf)^2),
-    sd_tech = sqrt(sum(lwage^2 * d_cdf_tech) - sum(lwage * d_cdf_tech)^2),
-    sd_cont = sqrt(sum(lwage^2 * d_cdf_cont) - sum(lwage * d_cdf_cont)^2)
-    )
-  
+  filter(!is.na(d_cdf))
+
+avg_empr <- sum(df_cdf$lwage * df_cdf$d_cdf) / sum(df_cdf$d_cdf)
+sd_empr <- sqrt(sum(df_cdf$lwage^2 * df_cdf$d_cdf) / sum(df_cdf$d_cdf) - avg_empr^2)
+
+avg_tech <- sum(df_cdf$lwage * df_cdf$d_cdf_tech) / sum(df_cdf$d_cdf_tech)
+sd_tech <- sqrt(sum(df_cdf$lwage^2 * df_cdf$d_cdf_tech) / sum(df_cdf$d_cdf_tech) - avg_tech^2)
+
+avg_cont <- sum(df_cdf$lwage * df_cdf$d_cdf_cont) / sum(df_cdf$d_cdf_cont)
+sd_cont <- sqrt(sum(df_cdf$lwage^2 * df_cdf$d_cdf_cont) / sum(df_cdf$d_cdf_cont) - avg_cont^2)
+
+lwage_t1 <- df %>% filter(YEAR == years[1]) %>% pull(lwage)
+weight_t1 <- df %>% filter(YEAR == years[1]) %>% pull(weight)
+lwage_t1_mean <- weighted.mean(lwage_t1, weight_t1)
+lwage_t1_sd <- sqrt(sum(weight_t1 * (lwage_t1 - lwage_t1_mean)^2))
+
+lwage_t2 <- df %>% filter(YEAR == years[2]) %>% pull(lwage)
+weight_t2 <- df %>% filter(YEAR == years[2]) %>% pull(weight)
+lwage_t2_mean <- weighted.mean(lwage_t2, weight_t2)
+lwage_t2_sd <- sqrt(sum(weight_t2 * (lwage_t2 - lwage_t2_mean)^2))
+
+
 # results data frame
 df_decompose <- data.frame(matrix(ncol = 5, nrow = 2))
 colnames(df_decompose) <- c("name", "total", "tech", "control", "unexplained")
 
 df_decompose[1, ] <- list("Std Dev", 
-                        df_results[2, "sd"] - df_results[1, "sd"],
-                        df_results[2, "sd"] - df_results[2, "sd_tech"],
-                        df_results[2, "sd_tech"] - df_results[2, "sd_cont"],
-                        df_results[2, "sd_cont"] - df_results[1, "sd"]
+                        lwage_t2_sd - lwage_t1_sd,
+                        lwage_t2_sd - sd_tech,
+                        sd_tech - sd_cont,
+                        sd_cont - lwage_t1_sd
 )
 
 df_decompose[2, ] <- list("pct", 
                         100,
-                        100 * (df_results[2, "sd"] - df_results[2, "sd_tech"]) / (df_results[2, "sd"] - df_results[1, "sd"]),
-                        100 * (df_results[2, "sd_tech"] - df_results[2, "sd_cont"]) / (df_results[2, "sd"] - df_results[1, "sd"]),
-                        100 * (df_results[2, "sd_cont"] - df_results[1, "sd"]) / (df_results[2, "sd"] - df_results[1, "sd"])
+                        100 * (lwage_t2_sd - sd_tech) / (lwage_t2_sd - lwage_t1_sd),
+                        100 * (sd_tech - sd_cont) / (lwage_t2_sd - lwage_t1_sd),
+                        100 * (sd_cont - lwage_t1_sd) / (lwage_t2_sd - lwage_t1_sd)
 )
 
 View(df_decompose)
--- code/tools.R
+++ code/tools.R
@@ -6,11 +6,11 @@ library(RSQLite)
 source("code/oes.R")
 
 # Read IPUMS CPS data and write to SQLite
-write_ipums_to_sql <- function() {
+write_cps_aces_to_sql <- function() {
   # READ IPUMS DATA ---------------------------------------------------------
   
-  cps_ddi_file <- "data/CPS/cps_00007.xml"
-  cps_data_file <- "data/CPS/cps_00007.dat"
+  cps_ddi_file <- "data/CPS/cps_aces.xml"
+  cps_data_file <- "data/CPS/cps_aces.dat"
   
   cps_ddi <- read_ipums_ddi(cps_ddi_file)
   cps_data <- read_ipums_micro(cps_ddi_file, data_file = cps_data_file)
@@ -19,7 +19,29 @@ write_ipums_to_sql <- function() {
   # SQLite ------------------------------------------------------------------
   
   # connection
-  db <- dbConnect(SQLite(), dbname="data_out/cps_data.sqlite")
+  db <- dbConnect(SQLite(), dbname="data_out/cps_aces.sqlite")
+  
+  # export
+  dbWriteTable(db, "cps", cps_data, overwrite = TRUE)
+  
+  # disconnect
+  dbDisconnect(db)
+}
+
+write_cps_org_to_sql <- function() {
+  # READ IPUMS DATA ---------------------------------------------------------
+  
+  cps_ddi_file <- "data/CPS/cps_org.xml"
+  cps_data_file <- "data/CPS/cps_org.dat"
+  
+  cps_ddi <- read_ipums_ddi(cps_ddi_file)
+  cps_data <- read_ipums_micro(cps_ddi_file, data_file = cps_data_file)
+  
+  
+  # SQLite ------------------------------------------------------------------
+  
+  # connection
+  db <- dbConnect(SQLite(), dbname="data_out/cps_org.sqlite")
   
   # export
   dbWriteTable(db, "cps", cps_data, overwrite = TRUE)
@@ -29,14 +51,14 @@ write_ipums_to_sql <- function() {
 }
 
 # Read CPS March data and construct the main dataset
-load_df <- function() {
+load_df_aces <- function() {
   # CPS March from IPUMS
-  cps_data <- sqldf("select * from cps", dbname = "data_out/cps_data.sqlite")
+  cps_data <- sqldf("select * from cps", dbname = paste0("data_out/cps_aces.sqlite"))
   
   # # GDP Deflators from https://fred.stlouisfed.org/series/GDPDEF
   # gdp_deflator <- read.csv("data/GDPDEF.csv", stringsAsFactors = F) %>%
   #   separate(DATE, c("y", "m", "d")) %>%
-  #   filter(m == "01") %>%
+  #   filter(d == "01") %>%
   #   column_to_rownames('y')
   
   # STEM, STEM-related
@@ -105,9 +127,88 @@ load_df <- function() {
   return(df)
 }
 
+# Read CPS March data and construct the main dataset
+load_df_org <- function() {
+  # CPS March from IPUMS
+  cps_data <- sqldf("select * from cps", dbname = paste0("data_out/cps_org.sqlite"))
+  
+  # GDP Deflators from https://fred.stlouisfed.org/series/GDPDEF
+  gdp_deflator <- read.csv("data/GDPDEF.csv", stringsAsFactors = F) %>%
+    separate(DATE, c("y", "m", "d")) %>%
+    filter(m == "01") %>%
+    column_to_rownames("y")
+  
+  # STEM, STEM-related
+  occ10 <- read.csv("data/SOC/occ10.csv", stringsAsFactors = F) %>%
+    mutate(is_stem = ifelse(stem %in% 2, 1, 0),
+           is_stem_related = ifelse(stem %in% c(1, 2), 1, 0)
+    ) %>%
+    rename(OCC2010 = OCC10LY)
+  
+  df <- cps_data %>%
+    # limit to full-time workers with non-zero salary income
+    filter(
+      HOURWAGE != 99.99, HOURWAGE > 0, EARNWT != 0, UHRSWORKORG != 999,
+      
+      # keep self-employed + private workers
+      CLASSWKR %in% c(10, 13, 14, 22)
+    ) %>%
+    
+    # weight
+    mutate(weight = EARNWT * UHRSWORKORG) %>%
+    group_by(YEAR) %>%
+    mutate(weight = weight / sum(weight)) %>%
+    ungroup() %>%
+    
+    # wage, lwage: inflation-adjusted
+    mutate(
+      wage = HOURWAGE * 100 / gdp_deflator[as.character(YEAR), "GDPDEF"],
+      lwage = log(wage)
+    ) %>%
+    
+    # schooling
+    mutate(
+      schooling = recode(unclass(EDUC),
+                         `2` = 0,
+                         `10` = 2,
+                         `20` = 5.5,
+                         `30` = 7.5,
+                         `40` = 9,
+                         `50` = 10,
+                         `60` = 11,
+                         `71` = 11.5,
+                         `73` = 12,
+                         `81` = 13,
+                         `91` = 14,
+                         `92` = 14,
+                         `111` = 16,
+                         `123` = 18,
+                         `124` = 18,
+                         `125` = 20
+      ),
+      experience = ifelse(AGE - schooling - 6 >= 0, AGE - schooling - 6, 0),
+      experience_sq = experience^2,
+      RACE = recode(unclass(RACE),
+                    `100` = "White",
+                    `200` = "Black",
+                    `300` = "American Indian",
+                    `651` = "Asian",
+                    `652` = "Asian",
+                    .default = "Other"
+      ),
+      married = ifelse(MARST == 1, 1, 0),
+      METAREA = factor(METAREA)
+    ) %>%
+    
+    # occupations: stem, stem-related
+    left_join(occ10 %>% select(OCC2010, is_stem, is_stem_related), by = "OCC2010")
+  
+  return(df)
+}
+
 # create mapping from INDLY to OES industry codes,
 # and calculate economy average and sd using the given set of occupations
-prepare_INDLY_conversion_census_to_naics <- function(years, tech_occupations, df_oes = NULL) {
+prepare_INDLY_conversion_census_to_naics <- function(years, tech_occupations, df_oes = NULL, cached = F, save = F) {
   # fetch empl, tech from oes using the census_code
   fetch_oes_empl_tech <- function(census_code, df) {
     # census_code: number
@@ -123,6 +224,11 @@ prepare_INDLY_conversion_census_to_naics <- function(years, tech_occupations, df
     ))
   }
   
+  # use cached?
+  if (cached) {
+    return(read.csv("data_out/INDLY_to_naics.csv", stringsAsFactors = F, fileEncoding = "UTF-8"))  
+  }
+  
   # construct df_oes only once
   if (is.null(df_oes)) {
     df_oes <- CompileOES(years, tech_occupations)
@@ -135,7 +241,11 @@ prepare_INDLY_conversion_census_to_naics <- function(years, tech_occupations, df
     for (year in tail(years, -1)) {
       df <- rbind(df, prepare_INDLY_conversion_census_to_naics(year, tech_occupations, df_oes))
     }
-    
+  
+    # save?
+    if (save) {
+      write.csv(df, "data_out/INDLY_to_naics.csv", fileEncoding = "UTF-8")
+    }
   } else {
     # Single year
     year <- years
